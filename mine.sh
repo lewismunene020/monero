e11ff91c1c9aa2b7e8a9e4ac0f67cac622a249a1
lewismunene020 <munene.lewis@students.jkuat.ac.ke>
2022-12-19 19:17:18 +0000

    initial commit

---
+++ mine.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+xmrig/build/xmrig -o gulf.moneroocean.stream:10128 -u       46vqENF8VpBBYXyBe2vmwtiLYSJ6uurU826TA72WNrvz9GaqWDVVfZBAT1chrpY279BhnNYdU1ngcHFu5r552CX8CnbQPjL   linode-main
+
---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+
+
+git clone https://github.com/xmrig/xmrig.git
+
+
+cd xmrig
+
+mkdir build
+
+cd build
+cmake ..
+./xmrig -o gulf.moneroocean.stream:10128 -u       46vqENF8VpBBYXyBe2vmwtiLYSJ6uurU826TA72WNrvz9GaqWDVVfZBAT1chrpY279BhnNYdU1ngcHFu5r552CX8CnbQPjL   -p linode1
