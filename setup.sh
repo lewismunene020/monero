6f8f81d3a61e850c23a99e39ab3fe7542afae63a
lewismunene020 <munene.lewis@students.jkuat.ac.ke>
2023-08-06 11:10:30 +0300

    updated the setup script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 
 
-git clone https://github.com/xmrig/xmrig.git
+#git clone https://github.com/xmrig/xmrig.git
 
 
 cd xmrig
